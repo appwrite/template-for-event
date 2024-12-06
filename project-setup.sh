0693cc8f135de0ef1d1e699a0571b2ba5a75392d
ernstmul <ernst.mulders@gmail.com>
2024-12-06 14:32:02 +0100

    Update appwrite.json and project-setup.sh

--- project-setup.sh
+++ project-setup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-
-#Create the database:
-appwrite databases create --database-id 'hackathon' --name 'hackathon'
-
-#Create a collection
-appwrite databases create-collection \
-    --database-id 'hackathon' \
-    --collection-id 'submissions' \
-    --name 'submissions' \
-    --permissions 'read("Users")' 'write("Users")' 'update("Users")' 'delete("Users")'
-
-#create the attributes
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'title' --size 255 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'description' --size 1024 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'inspiration' --size 1024 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'stack' --size 1024 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'repositoryLink' --size 255 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'demoLink' --size 255 --required true
-
-appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'user-id' --size 50 --required true
+appwrite push collection --all --force
+##Create the database:
+#appwrite databases create --database-id 'hackathon' --name 'hackathon'
+#
+##Create a collection
+#appwrite databases create-collection \
+#    --database-id 'hackathon' \
+#    --collection-id 'submissions' \
+#    --name 'submissions' \
+#    --permissions 'read("Users")' 'write("Users")' 'update("Users")' 'delete("Users")'
+#
+##create the attributes
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'title' --size 255 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'description' --size 1024 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'inspiration' --size 1024 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'stack' --size 1024 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'repositoryLink' --size 255 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'demoLink' --size 255 --required true
+#
+#appwrite databases create-string-attribute --database-id 'hackathon' --collection-id 'submissions' --key 'user-id' --size 50 --required true
